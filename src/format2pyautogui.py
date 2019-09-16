b384732b9557b94aa9f2b7e4d1f6ce9034f31ee0
Trung Hoang <39042628+trung-hn@users.noreply.github.com>
2019-09-16 09:36:10 -0400

    Update format2pyautogui.py

--- src/format2pyautogui.py
+++ src/format2pyautogui.py
@@ -4,18 +4,21 @@ Created on Thu Sep 12 14:07:08 2019
 
 @author: hoangt1
 """
+import os
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
 
 # Convert mouse events to pyautogui format. Called in on_press()
-def mouse2pyautogui(x = None, y = None, button = None, dx = None, dy = None):
+def mouse2pyautogui(x=None, y=None, button=None, dx=None, dy=None):
     # example: mouse2pyautogui(x = x, y = y, button = button)
     # Mouse clicking
     if button:
         if str(button) == "Button.left":
-            return f"click({x}, {y}, duration=.25 )\n"
+            return f"click({x}, {y}, duration=.25 )"
         elif str(button) == "Button.right":
-            return f"rightClick({x}, {y}, duration=.25 )\n"
+            return f"rightClick({x}, {y}, duration=.25 )"
         elif str(button) == "Button.middle":
-            return f"middleClick({x}, {y}, duration=.25 )\n"
+            return f"middleClick({x}, {y}, duration=.25 )"
 
     # example: mouse2pyautogui(x = x, y = y, dx = dx, dy = dy)
     # Mouse vertical scrolling
@@ -24,21 +27,31 @@ def mouse2pyautogui(x = None, y = None, button = None, dx = None, dy = None):
     # Mouse horizontal scrolling
     if dx: return f"pyautogui.hscroll({dx}, x = {x}, y = {y})"
 
+
 # Convert keyboard events to pyautogui format. Called in on_press()
-def keyboard2pyautogui(key_pressed = None, key_released = None):
+def keyboard2pyautogui(key_pressed=None, key_released=None):
     if key_pressed:
-        return f'typewrite({key})\n'
+        return f'typewrite({key_pressed})'
         # for special keys (enter, tab, shift, ...) you need to compare each of them
         # because pynput and pyautogui name them differently. More here:
         # https://pynput.readthedocs.io/en/latest/keyboard.html#pynput.keyboard.KeyCode
         # https://pyautogui.readthedocs.io/en/latest/keyboard.html#keyboard-keys
 
+
+def img2pyautogui(save_image_path, save_image_name):
+    image_code = f'{save_image_name} = r\'{save_image_path}\'\n'
+    f'select({save_image_name})\n'
+    f'sleep(1.5)\n\n'
+    return image_code
+
+
 # write recorded data to output file selected by user
-def write_output_file(recorded_data : list, save_file_path : str, header_file_path : str, import_header : bool) -> None:
+def write_output_file(recorded_data: list, save_file_path: str, header_file_path=None) -> None:
+    global dir_path
     print(recorded_data)
     with open(save_file_path, 'w') as file:
-
-        if import_header:
+        header_file_path = f'{dir_path}\header_py_auto.py'
+        if header_file_path:
             print_header(file, header_file_path)
 
         # clean up typewrite comments
@@ -49,15 +62,15 @@ def write_output_file(recorded_data : list, save_file_path : str, header_file_pa
     # Distinguish between comments and string input
     # Cleanup combination keys. We might need to get key_released as well.
 
-def print_header(file, header_file_path):
 
+def print_header(file, header_file_path):
     with open(header_file_path, 'r') as header_file:
         for line in header_file:
             file.write(line)
 
+
 # consolidate_typewrite statements from single letter to readable strings
 def consolidate_typewrite(file, recorded_data):
-
     # during consolidating, the last line shouldn't be typewrite
     recorded_data.append('### end of buffer###')
 
@@ -82,18 +95,10 @@ def consolidate_typewrite(file, recorded_data):
                 typewrite_content = ''
             file.write(line)
 
-
-#TODO Function that calls write_output_file should include the
-save_folder = r'C:\Users\Public\Documents\py_3\pyAutoGui\Trung_record'
-save_file_name = r'MoD_fileBuilder_auto.py'
-save_file_path = os.path.join(save_folder, save_file_name)
-
-header_file_path = r'C:\Users\Public\Documents\py_3\pyAutoGui\Trung_record\py_auto_header.py'
+# TODO Function that calls write_output_file should include the
 
 # first, request file name and display default path that it will be saved to
 # include option for import header
-import_header = False
-
 
 '''
 # User Notes
